cd6f1d753882666f7929e6056aae5f49c00a4e2d
Mikhael <mikhaelcutajar@gmail.com>
2021-03-04 14:30:39 +0000

    Initial Commit

---
+++ model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc_units = 512):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc_units (int): The size of all the hidden layers
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc_units)
+        self.fc2 = nn.Linear(fc_units, fc_units)
+        self.fc3 = nn.Linear(fc_units, fc_units)
+        self.fc4 = nn.Linear(fc_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        state = F.relu(self.fc1(state))
+        state = F.relu(self.fc2(state))
+        state = F.relu(self.fc3(state))
+        state = self.fc4(state)
+        return state
+    
